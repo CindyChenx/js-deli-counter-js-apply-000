bbc795b8c92e74114bece2a1669e0fdd008351f7
outstanding-bolt-9487 <chen_cx0128@hotmail.com>
2019-05-21 01:43:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var cindyDeli = [ ];
 
 function takeANumber(cindyDeli, "Ada"){
   cindyDeli.push("Ada");
-  console.log(`Welcome, Ada. You are number ${cindyDeli.length+1} in line.`)
+  return `Welcome, Ada. You are number ${cindyDeli.length+1} in line.`
 }
